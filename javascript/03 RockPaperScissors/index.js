47e8ed956e08d023086dfe683fce1c224b857ed3
77dfeba <349841726@qq.com>
2023-09-17 20:17:34 +0800

    功能基本完成，待测试

--- javascript/03 RockPaperScissors/index.js
+++ javascript/03 RockPaperScissors/index.js
@@ -6,30 +6,26 @@ const gestureSelect = document.querySelector("#gesture-select")
 const resultOfThisRound = document.querySelector(".result-of-this-round")
 
 const GestureImg = {
-    rock: "./images/rock.png",
-    scissor: "./images/scissor.png",
-    paper: "./images/paper.png",
+    rock: "./images/rock.png", scissor: "./images/scissor.png", paper: "./images/paper.png",
 }
 
 const GesturesNames = ["rock", "scissor", "paper"];
 
 const Name = {
-    computer: "机器人",
-    my: "你"
+    computer: "机器人", my: "你"
 }
 
 const myScore = {
-    win: 0,
-    lose: 0
+    win: 0, lose: 0
 }
 
-const computeScore = {
-    win: 0,
-    lose: 0
+const robotScore = {
+    win: 0, lose: 0
 }
+
 /* 3局胜利后结束 */
 const maxEndRoundNum = 3
-let round = 1
+let roundNum = 1
 
 
 function initGame() {
@@ -46,28 +42,91 @@ function initGame() {
     })
 }
 
-function gesture() {
-
-}
 
 function randomGesture() {
     return GesturesNames[Math.random() * 3]
 }
 
-function renderMyGesture() {
-
+function renderMyGesture(gesture) {
+    const gestureImg = GestureImg[gesture]
+    const wrapper = document.querySelector(".my .gesture")
+    wrapper.innerHTML = `<img class="my-img" src=${gestureImg}  alt="我的手势"/>`
 }
 
 function renderComputerGesture() {
-    const computerGesture = randomGesture()
+    const gestureName = randomGesture()
+    const gestureImg = GestureImg[gestureName]
+    const wrapper = document.querySelector(".computer .gesture")
+    wrapper.innerHTML = `<img class="computer-img" src=${gestureImg} alt=机器人手势""/>`
+    return gestureName
+}
+
+function renderScore() {
+    const computerScore = document.querySelector(".computer .score");
+    const myScore = document.querySelector(".my .score");
+    computerScore.innerHTML = `胜：${computerScore.win} | 负：${computerScore.lose}`;
+    myScore.innerHTML = `胜：${myScore.win} | 负：${myScore.lose}`;
+}
+
+function renderRoundInfo(roundNum) {
+    const roundDom = document.querySelector(".round");
+    roundDom.innerHTML = `第${roundNum}回合`;
 }
 
 function judgeRound(my, computer) {
+    let roundResult = ''
+    if (my === computer) {
+        roundResult = '平局'
+    } else {
+        let isMyWin = true
+        if (computer === "rock") {
+            isMyWin = my !== "scissor";
+        } else if (computer === "scissor") {
+            isMyWin = my === "rock";
+        } else {
+            isMyWin = my !== "rock";
+        }
 
+        if (isMyWin) {
+            roundResult = "本回合你赢";
+            myScore.win++;
+            robotScore.lose++;
+        } else {
+            roundResult = "本回合机器人赢";
+            myScore.lose++;
+            robotScore.win++;
+        }
+    }
+
+    /* 渲染结果 */
+    resultOfThisRound.innerHTML = roundResult
+    renderScore()
+    renderRoundInfo()
+
+    /* 处理游戏结果 */
+    roundNum++;
+    if (robotScore.win >= maxEndRoundNum || myScore.win >= maxEndRoundNum) {
+        if (robotScore.win > myScore.win) {
+            endGame("computer");
+        } else if (myScore.win > robotScore.win) {
+            endGame("my");
+        } else {
+            endGame()
+        }
+    }
 }
 
-function endGame(winner) {
 
+function endGame(winner) {
+    gestureDialog.close()
+    startRoundBtn.style.display = 'none'
+    const resultOfTotalRound = document.querySelector(".result-of-total-round");
+
+    if (!winner) {
+        resultOfTotalRound.innerHTML = "不错嘛，平局了";
+    } else {
+        resultOfTotalRound.innerHTML = `(≧v≦)o~~好棒，恭喜${Name[winner]}获得胜利！`;
+    }
 }
 
 initGame()
