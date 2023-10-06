4760395818555b672d24fe966a1dc0f84144deaf
77dfeba <349841726@qq.com>
2023-10-06 23:45:17 +0800

    作业4：js方法设计

---
+++ javascript/04 MovingCar/index.js
@@ -0,0 +1,56 @@
+// 按钮常量和车存储位置
+const keyCode = {
+    left: 37,
+    right: 39,
+    up: 38,
+    down: 40,
+}
+const carTypeImgPath = {
+    tractors: './images/car1.png',
+    truck: './images/car2.png',
+    motor: './images/car3.png',
+    roadster: './images/car4.png',
+    electricMotor: './images/car5.png',
+    bike: './images/car6.png'
+}
+
+// 全局相关DOM和变量
+let speedSelectDom = null
+let carTypeSelectDom = null
+let gameMapDom = null
+let carDom = null
+
+let carDirection = 'right'
+
+/* 游戏初始化 */
+function init() {
+
+}
+
+
+/* 变化监听 */
+function carSpeedChangeListener() {
+
+}
+
+function carTypeChangeListener() {
+
+}
+
+/* 车辆控制*/
+function moveKeyListener() {
+
+}
+
+/* 车辆渲染 */
+function moveCar() {
+
+}
+
+function changeCarHead() {
+
+}
+
+function renderCar() {
+
+}
